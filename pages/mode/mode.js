1227e47d4dce58f04976657f611c202518dab617
HuziG <1067408814@qq.com>
2020-05-10 18:47:45 +0800

    修改词库选择跳转方式

--- pages/mode/mode.js
+++ pages/mode/mode.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: dingjia z
+ * @Date: 2020-04-23 21:54:21
+ * @LastEditors: dingjia z
+ * @LastEditTime: 2020-05-10 18:45:41
+ */
 // pages/mode/mode.js
 import { WordInfoModel } from "../../models/wordInfoModel.js";
 
@@ -7,19 +15,19 @@ const App = getApp();
 
 Page({
   data: {
-    skinStyle: App.globalData.skinStyle
+    skinStyle: App.globalData.skinStyle,
   },
 
-  onLoad: function(options) {
+  onLoad: function (options) {
     this.checkBackBtn(options);
-    this.setSkipStyle()
+    this.setSkipStyle();
   },
 
   setMode(e) {
     // 设置mode
-    wordInfoModel.setWordMode(e.detail.mode).then(res => {
-      wx.redirectTo({
-        url: "/pages/home/home"
+    wordInfoModel.setWordMode(e.detail.mode).then((res) => {
+      wx.reLaunch({
+        url: "/pages/home/home",
       });
     });
   },
@@ -28,18 +36,18 @@ Page({
     // 检测是否需要显示nav的返回按钮
     if (options.frompage === "homepage") {
       this.setData({
-        navMode: 1
+        navMode: 1,
       });
     } else if (options.frompage === undefined) {
       this.setData({
-        navMode: 0
+        navMode: 0,
       });
     }
   },
 
   setSkipStyle() {
     this.setData({
-      skinStyle: App.globalData.skinStyle
-    })
-  }
+      skinStyle: App.globalData.skinStyle,
+    });
+  },
 });
